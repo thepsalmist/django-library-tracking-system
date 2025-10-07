8c0e40386374fa9816923e6674b855948d4ce04a
Xavier Frankline <xf.xavierfrank@gmail.com>
2025-10-07 17:42:47 +0300

    chore: implement chore_tasks
    List comprehension and filter

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,12 @@
 import random
-# rand_list =
 
-# list_comprehension_below_10 =
+numbers = [random.randint(1,20) for _ in range (10)]
+print(numbers)
 
-# list_comprehension_below_10 =
+# List comprehension
+n_below_10_list_comprehension = [n for n in numbers if n < 10]
+print(n_below_10_list_comprehension)
+
+# Filter
+n_below_10_filter = list(filter(lambda n:n <10, numbers))
+print(n_below_10_filter)
